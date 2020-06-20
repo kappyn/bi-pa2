610e0fc65b20d2d132617a69fc80c02ea0c1d8fa
kappyn <kevin.kroupa@email.cz>
2020-06-20 15:09:39 +0200

    makefile update no. 2

--- sw/src/query/CSetQuery.cpp
+++ sw/src/query/CSetQuery.cpp
@@ -1,5 +1,9 @@
 #include "CSetQuery.hpp"
 
+/*
+ * Searches trough the database and saves the table references.
+ * If any error occurs (table not found, invalid columns, data types), false is returned.
+ */
 bool CSetQuery::SaveTableReferences ( ) {
 	if ( ( m_Operands.first.m_TRef = m_Database.GetTable( m_TableNames.first ) ) != nullptr ) { }
 	else if ( ( m_Operands.first.m_QRef = m_Database.GetTableQ( m_TableNames.first ) ) != nullptr ) {
@@ -28,6 +32,10 @@ bool CSetQuery::SaveTableReferences ( ) {
 	return true;
 }
 
+/*
+ * Validates individual columns types of two operands.
+ * Method returns true if all columns have same data types.
+ */
 bool CSetQuery::ValidateColumnTypes ( ) const {
 	if ( ! m_Operands.first.m_TRef || ! m_Operands.second.m_TRef )
 		return false;
